a29f045f3a1f9ba634d89a012af7fe9278f1cb24
dghosh <debz.winch@gmail.com>
2016-08-07 10:34:19 +0100

    added getImages function

--- js/MainController.js
+++ js/MainController.js
@@ -40,16 +40,21 @@ profile.controller('MainController',
 		controller: function($scope, $http) {
 			var accessToken = "280227547.4ffd8a3.43c970c362be40d6bee37279ec6f5c81";
 			var url = "https://api.instagram.com/v1/users/self/media/recent/?callback=JSON_CALLBACK&access_token=" + accessToken;
-			var images = $http.jsonp(url)
-				.success(function(data) {
-					var imageData = data.data;
-					imageData.forEach((image) => {
-						console.log(image.images.thumbnail);
+
+			$scope.getImages = function() {
+				
+				var images = $http.jsonp(url)
+					.success(function(data) {
+						var imageData = data.data;
+						imageData.forEach((image) => {
+							console.log(image.images.thumbnail);
+						});
+					})
+					.error(function(data, status) {
+						console.log(status);
 					});
-				})
-				.error(function(data, status) {
-					console.log(status);
-				});
+			}
+
 		}
 	}
 })
